43a4112264a7f6f847e3997131fc9c703a31bd1c
Willem Mali <willem.mali@gmail.com>
2016-07-18 21:47:43 +0200

    backup

--- debian.sh
+++ debian.sh
@@ -1,9 +1,79 @@
 #!/bin/sh -e
-# config
+# version info
+version() {
+    echo "0.0.1"
+    exit 0
+}
+
+# debian.sh usage
+usage() {
+    echo "Usage: debian.sh [OPTIONS]"
+    echo "Bootstrap a salt master on a Debian system."
+    echo ""
+    echo "Mandatory arguments to long options are mandatory for short options too."
+    echo "-f, --force       don't nag"
+    echo "-u, --user=USER   install as specific user"
+    echo "    --help        display this help and exit"
+    echo "    --version     show version information and exit"
+    exit 0
+}
+
+# does a command exist?
+command_exists() {
+    type "$1" > /dev/null 2>&1
+}
+
+sudo_command_exists() {
+    echo "Checking if command '$1' exists..."
+    echo "FIXME: this function doesn't work and is ignored."
+    # sudo sh -c 'type "$1"'
+}
+
+# ensure we can run certain commands as user
+requiredcommands="echo command sudo getopts"
+for command in $requiredcommands
+do
+    command_exists "$command" || (
+        echo "user can't access required command: $command"
+        exit;
+    )
+done
+
+# nag for safety
+if [ ("$1" != "-f" || getuid == "0") ]; then
+    echo "This script runs commands using sudo. Use at your own peril."
+    echo "To disable this prompt, use the -f option."
+    echo ""
+    while true; do
+        read -p "Do you wish to run this SaltStack deployment script? [Y/N]: " answer
+        case $answer in
+            [Yy]* ) break;;
+            [Nn]* ) exit;;
+            * ) echo "Y/N?"
+        esac
+    done
+fi
+
+# ensure we can run certain commands as root
+requiredrootcommands="rm cp chown useradd groupadd"
+for command in $requiredrootcommands do
+do
+    sudo_command_exists "$command" || (
+        echo "root user can't access required command: $command"
+        exit;
+    )
+done
+
+# default config
 saltuser="salt"
 saltgroup="salt"
 
+# directories that should be owned by the salt user
 saltowneddirs="/etc/salt /var/cache/salt /var/log/salt /var/run/salt"
+
+
+# parse commandline options
+
 
 # install salt
 echo "Installing required packages..."
@@ -39,3 +109,5 @@ sudo chown salt:salt /etc/salt/master
 sudo cp minion /etc/salt/
 sudo chown salt:salt /etc/salt/minion
 echo "Done."
+
+
