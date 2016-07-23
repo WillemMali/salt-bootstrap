ba8b9f8f008cb904238a3233b11c29f73cc84e6f
Willem Mali <willem.mali@gmail.com>
2016-07-23 19:33:42 +0200

    backup

--- debian.sh
+++ debian.sh
@@ -1,7 +1,9 @@
-#!/bin/sh -e
+#!/bin/sh -eu
+# stop on error or unset variables
+
 # version info
 version() {
-    echo "0.0.1"
+    echo "0.0.2"
     exit 0
 }
 
@@ -11,103 +13,169 @@ usage() {
     echo "Bootstrap a salt master on a Debian system."
     echo ""
     echo "Mandatory arguments to long options are mandatory for short options too."
-    echo "-f, --force       don't nag"
-    echo "-u, --user=USER   install as specific user"
-    echo "    --help        display this help and exit"
-    echo "    --version     show version information and exit"
+    echo "-f   don't nag"
+    echo "-o   directories to chown for the salt user+group"
+    echo "-O   same as -o, but don't chown default directories"
+    echo "-u   install as specific user"
+    echo "-v   use verbose output (-vvv for most verbose)"
+    echo "-q   don't output anything"
+    echo ""
+    echo "-h   display this help and exit"
+    echo "-V   show version information and exit"
     exit 0
 }
 
-# does a command exist?
-command_exists() {
-    type "$1" > /dev/null 2>&1
+
+# helper functions
+error() {
+    text=${1:-`cat </dev/stdin`}
+    verbose_printer 1 $text
 }
 
-sudo_command_exists() {
-    echo "Checking if command '$1' exists..."
-    echo "FIXME: this function doesn't work and is ignored."
-    # sudo sh -c 'type "$1"'
+warn() {
+    text=${1:-`cat </dev/stdin`}
+    verbose_printer 2 $text
 }
 
-# ensure we can run certain commands as user
-requiredcommands="echo command sudo getopts"
-for command in $requiredcommands
-do
-    command_exists "$command" || (
-        echo "user can't access required command: $command"
-        exit;
-    )
-done
+info() {
+    text=${1:-`cat </dev/stdin`}
+    verbose_printer 3 $text
+}
 
-# nag for safety
-if [ ("$1" != "-f" || getuid == "0") ]; then
+verbose_printer() {
+    [ $1 -gt $verbosity ] || ( echo $text >&2 || true )
+}
+
+
+# default config
+confirm=true
+verbosity=3
+saltuser="salt"
+saltgroup="salt"
+# directories that should be owned by the salt user
+saltowneddirs="/etc/salt /var/cache/salt /var/log/salt /var/run/salt"
+
+
+# process commandline arguments
+invalid_argument() {
+    error "Invalid option '$0' passed, aborting."
+    exit
+}
+
+process_arguments() {
+    OPTIND=1
+    while getopts ":fhvu:" opt; do
+        info "Parsing option '$opt'..."
+        case $opt in
+            h)
+                usage
+                ;;
+            o)
+                saltowneddirs="$saltowneddirs $OPTARG"
+                ;;
+            O)
+                saltowneddirs="$OPTARG"
+                ;;
+            u)
+                saltuser=$2
+                ;;
+            y)
+                confirm=false
+                ;;
+            V)
+                version
+                ;;
+            \?)
+                invalid_argument $OPTARG
+                ;;
+        esac
+    done
+}
+
+echo "Processing commandline arguments..."
+process_arguments $@
+echo "Done."
+
+
+
+# prevent foot-shooting
+confirm() {
     echo "This script runs commands using sudo. Use at your own peril."
     echo "To disable this prompt, use the -f option."
     echo ""
     while true; do
         read -p "Do you wish to run this SaltStack deployment script? [Y/N]: " answer
         case $answer in
-            [Yy]* ) break;;
-            [Nn]* ) exit;;
+            [Yy]* ) return;;
+            [Nn]* ) echo "Aborting."; exit;;
             * ) echo "Y/N?"
         esac
     done
-fi
+}
 
-# ensure we can run certain commands as root
-requiredrootcommands="rm cp chown useradd groupadd"
-for command in $requiredrootcommands do
+[ $confirm == false ] || confirm
+
+
+
+
+# can the current user access required commands?
+command_exists() {
+    type "$1" > /dev/null 2>&1 || return 1
+}
+
+
+# ensure we have certain commands available as user
+requiredcommands="echo command sudo getopts anu rm cp chown useradd groupadd"
+for command in $requiredcommands
 do
-    sudo_command_exists "$command" || (
-        echo "root user can't access required command: $command"
+    info "Checking if required command '$command' exists..."
+    command_exists "$command" || {
+        error "Can't access required command '$command', aborting."
         exit;
-    )
+    }
+    info "Done."
 done
 
-# default config
-saltuser="salt"
-saltgroup="salt"
 
-# directories that should be owned by the salt user
-saltowneddirs="/etc/salt /var/cache/salt /var/log/salt /var/run/salt"
-
-
-# parse commandline options
+exit
 
 
 # install salt
-echo "Installing required packages..."
+info "Installing required packages..."
 sudo apt-get install salt-master salt-minion
-echo "Done."
+info "Done."
 
 # add users and groups
-echo "Adding $saltgroup group..."
+info "Adding $saltgroup group..."
 sudo groupadd -f "$saltgroup"
-echo "Done."
+info "Done."
 
-echo "Adding ${saltuser} user..."
+info "Adding ${saltuser} user..."
 id -u $saltuser || sudo useradd -g "$saltgroup" "$saltuser"
-echo "Done."
+info "Done."
 
 # take ownership of directories to be used by salt
-echo "Taking ownership of salt directories..."
+info "Taking ownership of salt directories..."
 for directory in $saltowneddirs
 do
-    echo "$directory"
+    info "$directory"
     sudo chown -R "$saltgroup" $directory
 done
-echo "Done."
+info "Done."
 
 # deploy salt settings
-echo "Deploying salt config files"
-sudo rm -f /etc/salt/master
-sudo rm -f /etc/salt/minion
+info "Deploying salt config files..."
+if [ -f master ]; then
+    sudo rm -f /etc/salt/master
+    sudo cp master /etc/salt/
+    sudo chown salt:salt /etc/salt/master
+fi
 
-sudo cp master /etc/salt/
-sudo chown salt:salt /etc/salt/master
+if [ -f minion ]; then
+    sudo rm -f /etc/salt/minion
+    sudo cp minion /etc/salt/
+    sudo chown salt:salt /etc/salt/minion
+fi
+info "Done."
 
-sudo cp minion /etc/salt/
-sudo chown salt:salt /etc/salt/minion
-echo "Done."
-
-
+sudo -u $saltuser salt-key -L
